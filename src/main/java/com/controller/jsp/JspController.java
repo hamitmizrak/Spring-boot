58a9f016bf5fbc83e8f6ad1bd272620fa1c6f082
HamitMizrak <hamitmizrak@gmail.com>
2021-04-24 17:10:59 +0300

    Web Page Jsp File Running

---
+++ src/main/java/com/controller/jsp/JspController.java
@@ -0,0 +1,14 @@
+package com.controller.jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JspController {
+
+	@GetMapping("/jsp")
+	public String getIndexJsp() {
+		
+		return "index";
+	}
+}
