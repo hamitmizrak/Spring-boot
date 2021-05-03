f6d79efeb3b3d9115817d765306f55ead8cd6559
HamitMizrak <hamitmizrak@gmail.com>
2021-05-03 12:43:48 +0300

    Rest Default

--- src/main/java/com/hamitmizrak/rest/RestServicesControllerTutorials.java
+++ src/main/java/com/hamitmizrak/rest/RestServicesControllerTutorials.java
@@ -14,4 +14,24 @@ public class RestServicesControllerTutorials {
 		return stringJson;
 	}
 
+	// Defaultu değiştirmek
+	// http:localhost:9292/rest/json/string
+	// @GetMapping(value = "/rest/json/stringDefault", produces =
+	// org.springframework.http.MediaType.APPLICATION_JSON_UTF8)
+	@GetMapping("/rest/json/stringDefault")
+	public String getStringJsonDefault() {
+		String stringJson = "{\r\n" + "  \r\n" + "  \"adi\":\"Hamit\",\r\n" + "  \"soyadi\":\"Mızrak\"\r\n" + "  \r\n"
+				+ "}";
+		return stringJson;
+	}
+
+	// defaultta Json olarak gelmektedir.
+	// http:localhost:9292/rest/json/object
+	@GetMapping("/rest/json/object")
+	public Violist getObjectJson() {
+		Violist violist = new Violist(100, "Hamit", "Mızrak", "35", "Malatya");
+		violist.setViolistCountry("Malatya".toUpperCase());
+		return violist;
+	}
+
 }
